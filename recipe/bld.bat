26365c39f4d03dbcadedfa623e9040f2695eaa9a
Brian Rose <brose@albany.edu>
2026-05-04 08:34:27 -0400

    who knows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,5 @@
+setlocal EnableDelayedExpansion
+
 REM Make flang embed flang_rt.runtime.static.lib (conda-forge ships this but not
 REM the dynamic variant).
 set "FFLAGS=-fms-runtime-lib=static"
@@ -5,11 +7,17 @@ set "FFLAGS=-fms-runtime-lib=static"
 REM Use lld-link instead of MSVC link.exe: lld-link can read llvm-ar .a archives.
 set "CC_LD=lld-link"
 
-REM clang_rt.builtins is at lib\clang\21\lib\windows\ (confirmed, no Library\ prefix).
-REM flang_rt.runtime.static.lib location is uncertain; search all plausible paths.
+REM clang_rt.builtins is confirmed at lib\clang\21\lib\windows\ (no Library\ prefix,
+REM as seen in meson's auto-generated flag path from the build log).
 set "CLANG_RT_DIR=%BUILD_PREFIX%\lib\clang\21\lib\windows"
-set "LIB=%CLANG_RT_DIR%;%BUILD_PREFIX%\Library\lib\clang\21\lib\windows;%BUILD_PREFIX%\Library\lib;%BUILD_PREFIX%\lib;%LIB%"
-set "LIBPATH=%CLANG_RT_DIR%;%BUILD_PREFIX%\Library\lib\clang\21\lib\windows;%BUILD_PREFIX%\Library\lib;%BUILD_PREFIX%\lib;%LIBPATH%"
+set "LIB=%CLANG_RT_DIR%;%BUILD_PREFIX%\Library\lib;%LIB%"
+set "LIBPATH=%CLANG_RT_DIR%;%BUILD_PREFIX%\Library\lib;%LIBPATH%"
+
+REM flang_rt.runtime.static.lib location varies; search the whole build prefix and
+REM add whatever directory contains it to the linker library search path.
+for /f "tokens=*" %%f in ('dir /s /b "%BUILD_PREFIX%\flang_rt.runtime.static.lib" 2^>nul') do set "FLANG_RT_DIR=%%~dpf"
+if defined FLANG_RT_DIR set "LIB=!FLANG_RT_DIR!;!LIB!"
+if defined FLANG_RT_DIR set "LIBPATH=!FLANG_RT_DIR!;!LIBPATH!"
 
 REM meson generates Fortran runtime flags as '-Wl,-defaultlib:...' (compiler-driver
 REM format) which lld-link silently ignores, leaving __floatsitf etc. unresolved.
